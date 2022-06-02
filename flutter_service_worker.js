d3b51e0844e025ad83014a2f2d8b20715f0b1732
kalisynth <cade2002@gmail.com>
2022-06-02 11:37:45 +1000

    interests issue 1 fix
    removed unused audio only call in clubs
    updated and confirmed fix for interests issue 1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -59,10 +59,10 @@ const RESOURCES = {
 "icons/ms-icon-150x150.png": "5022c2bf22710fbb197c46a3ca08e0d1",
 "icons/ms-icon-310x310.png": "be9497c21925e4d091ad81514e188131",
 "icons/ms-icon-70x70.png": "602874a6f063b1342f7436d9cf627ba2",
-"index.html": "001b0cdc4b5c92b6e26d0f41be166740",
-"/": "001b0cdc4b5c92b6e26d0f41be166740",
+"index.html": "aca79a412204e50cdf71b0ca75a728fb",
+"/": "aca79a412204e50cdf71b0ca75a728fb",
 "jsfunctions.js": "527047b4742849d7741d0e74f3aea90e",
-"main.dart.js": "6a62c76aac3a5539da609eaeee61087d",
+"main.dart.js": "7310380c2a53ce3ff69314f5a1bd0e9e",
 "manifest.json": "b5a8a967df78e46328f4db6abd07f563",
 "version.json": "4162556d16194ef737c3d9b78eb06ca9"
 };
