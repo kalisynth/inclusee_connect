f9e5ff26c2eada2e82da5c6cde135349375aee27
kalisynth <cade2002@gmail.com>
2022-06-07 11:23:16 +1000

    add url function

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -70,10 +70,10 @@ const RESOURCES = {
 "icons/ms-icon-150x150.png": "5022c2bf22710fbb197c46a3ca08e0d1",
 "icons/ms-icon-310x310.png": "be9497c21925e4d091ad81514e188131",
 "icons/ms-icon-70x70.png": "602874a6f063b1342f7436d9cf627ba2",
-"index.html": "5cedbf4375f8d867aacdc725b467a2a0",
-"/": "5cedbf4375f8d867aacdc725b467a2a0",
+"index.html": "14b354880995cbe53492c52e1d214b4e",
+"/": "14b354880995cbe53492c52e1d214b4e",
 "jsfunctions.js": "527047b4742849d7741d0e74f3aea90e",
-"main.dart.js": "c203c3bf0782826bd1283fd87192d661",
+"main.dart.js": "d6d025257b9c8b8d2367cf0f91f0cd9c",
 "manifest.json": "b5a8a967df78e46328f4db6abd07f563",
 "version.json": "4162556d16194ef737c3d9b78eb06ca9"
 };
