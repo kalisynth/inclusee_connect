ffafc5b2367d281cfc4d3601beb5907957b5d3de
kalisynth <cade2002@gmail.com>
2023-01-09 10:55:22 +1000

    timetable update
    fixed wellness and singing

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -179,10 +179,10 @@ const RESOURCES = {
 "icons/ms-icon-150x150.png": "5022c2bf22710fbb197c46a3ca08e0d1",
 "icons/ms-icon-310x310.png": "be9497c21925e4d091ad81514e188131",
 "icons/ms-icon-70x70.png": "602874a6f063b1342f7436d9cf627ba2",
-"index.html": "4764d647613106943a8a751a56937d5d",
-"/": "4764d647613106943a8a751a56937d5d",
+"index.html": "635c7eacbdcffe7ebf107190d4375773",
+"/": "635c7eacbdcffe7ebf107190d4375773",
 "jsfunctions.js": "527047b4742849d7741d0e74f3aea90e",
-"main.dart.js": "43c1cac7067096885011ee75486b8650",
+"main.dart.js": "cc4c838d0f14d8cf4983785c6534db59",
 "manifest.json": "b5a8a967df78e46328f4db6abd07f563",
 "version.json": "e640fb6bb2a22b070c3f374f8c8ae980"
 };
