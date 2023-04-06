c39688d704266c25812d1f4a3d5d75d4198013f8
kalisynth <cade2002@gmail.com>
2023-04-06 17:22:41 +1000

    link fixed maybe

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -199,10 +199,10 @@ const RESOURCES = {
 "icons/ms-icon-150x150.png": "5022c2bf22710fbb197c46a3ca08e0d1",
 "icons/ms-icon-310x310.png": "be9497c21925e4d091ad81514e188131",
 "icons/ms-icon-70x70.png": "602874a6f063b1342f7436d9cf627ba2",
-"index.html": "6fe122f44166f6ad8e8f4e88a8dac58f",
-"/": "6fe122f44166f6ad8e8f4e88a8dac58f",
+"index.html": "05927a1443f8abd4830875cdbfa9e4e9",
+"/": "05927a1443f8abd4830875cdbfa9e4e9",
 "jsfunctions.js": "527047b4742849d7741d0e74f3aea90e",
-"main.dart.js": "09991d6794c828d37fc7cde138af2914",
+"main.dart.js": "8315b34ad9d49a7eae65bc0413559181",
 "manifest.json": "b5a8a967df78e46328f4db6abd07f563",
 "version.json": "e640fb6bb2a22b070c3f374f8c8ae980"
 };
