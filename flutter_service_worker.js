550eefaf69609cda4e9fa5fcb94915c60fcf2ddb
kalisynth <cade2002@gmail.com>
2023-05-05 15:05:34 +1000

    bible fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -203,10 +203,10 @@ const RESOURCES = {
 "icons/ms-icon-150x150.png": "5022c2bf22710fbb197c46a3ca08e0d1",
 "icons/ms-icon-310x310.png": "be9497c21925e4d091ad81514e188131",
 "icons/ms-icon-70x70.png": "602874a6f063b1342f7436d9cf627ba2",
-"index.html": "ccc0e0f9deacd56012b8c902c75a0e5a",
-"/": "ccc0e0f9deacd56012b8c902c75a0e5a",
+"index.html": "d0e747bdd20d74d4c2d2ae916b0de0c1",
+"/": "d0e747bdd20d74d4c2d2ae916b0de0c1",
 "jsfunctions.js": "527047b4742849d7741d0e74f3aea90e",
-"main.dart.js": "ff87f64dd6b9362a12a7bbbae8d533e6",
+"main.dart.js": "edeed2fcb27968f1cded276724d77f99",
 "manifest.json": "b5a8a967df78e46328f4db6abd07f563",
 "version.json": "e640fb6bb2a22b070c3f374f8c8ae980"
 };
