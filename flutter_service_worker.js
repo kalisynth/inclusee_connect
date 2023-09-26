169f29daf7fb6941d04b1f2c0a111afea68e3209
kalisynth <cade2002@gmail.com>
2023-09-26 11:00:04 +1000

    oct and public holiday update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -256,10 +256,10 @@ const RESOURCES = {
 "icons/ms-icon-150x150.png": "5022c2bf22710fbb197c46a3ca08e0d1",
 "icons/ms-icon-310x310.png": "be9497c21925e4d091ad81514e188131",
 "icons/ms-icon-70x70.png": "602874a6f063b1342f7436d9cf627ba2",
-"index.html": "9bb292c1a845d9f7e5ba6e8f6e4f378c",
-"/": "9bb292c1a845d9f7e5ba6e8f6e4f378c",
+"index.html": "e40d04943f2897213f7c74be1061a57b",
+"/": "e40d04943f2897213f7c74be1061a57b",
 "jsfunctions.js": "527047b4742849d7741d0e74f3aea90e",
-"main.dart.js": "7d525e13b22f723884454004654db799",
+"main.dart.js": "d0019eba996be6b23765c065cf58e122",
 "manifest.json": "b5a8a967df78e46328f4db6abd07f563",
 "version.json": "e640fb6bb2a22b070c3f374f8c8ae980"
 };
