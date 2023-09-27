955a6cf7da2280b293dddec852409ca36571b593
kalisynth <cade2002@gmail.com>
2023-09-27 15:24:26 +1000

    timetable update 2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -256,10 +256,10 @@ const RESOURCES = {
 "icons/ms-icon-150x150.png": "5022c2bf22710fbb197c46a3ca08e0d1",
 "icons/ms-icon-310x310.png": "be9497c21925e4d091ad81514e188131",
 "icons/ms-icon-70x70.png": "602874a6f063b1342f7436d9cf627ba2",
-"index.html": "ee07668c060a8744c90c6b2cf8f25ef6",
-"/": "ee07668c060a8744c90c6b2cf8f25ef6",
+"index.html": "360f12aadb2e63c244bab852cd8119dd",
+"/": "360f12aadb2e63c244bab852cd8119dd",
 "jsfunctions.js": "527047b4742849d7741d0e74f3aea90e",
-"main.dart.js": "b8b2285b07438fd5548726e519b6c642",
+"main.dart.js": "74112c6b9ac3b86c11f24bca61e0705e",
 "manifest.json": "b5a8a967df78e46328f4db6abd07f563",
 "version.json": "e640fb6bb2a22b070c3f374f8c8ae980"
 };
