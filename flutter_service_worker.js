799ef8307dab3e22ff0baea4de6386f1fa234602
kalisynth <cade2002@gmail.com>
2023-10-12 13:53:47 +1000

    calendar update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -259,10 +259,10 @@ const RESOURCES = {
 "icons/ms-icon-150x150.png": "5022c2bf22710fbb197c46a3ca08e0d1",
 "icons/ms-icon-310x310.png": "be9497c21925e4d091ad81514e188131",
 "icons/ms-icon-70x70.png": "602874a6f063b1342f7436d9cf627ba2",
-"index.html": "96400a85b2887c77f05962bd160b267e",
-"/": "96400a85b2887c77f05962bd160b267e",
+"index.html": "bda89b5ccc66b6289d6b6bae6c3ba749",
+"/": "bda89b5ccc66b6289d6b6bae6c3ba749",
 "jsfunctions.js": "527047b4742849d7741d0e74f3aea90e",
-"main.dart.js": "bf259be753e0a8c2158a21c6390213da",
+"main.dart.js": "95a0941be813480520ffe5605e027c09",
 "manifest.json": "b5a8a967df78e46328f4db6abd07f563",
 "version.json": "e640fb6bb2a22b070c3f374f8c8ae980"
 };
