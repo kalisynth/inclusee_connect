c75855d815f47104b238ed930da30fc6a12bdf68
kalisynth <cade2002@gmail.com>
2024-07-24 14:29:05 +1000

    timetable update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -158,10 +158,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "706d00d75983225f828021cd8e89139c
 "icons/ms-icon-150x150.png": "5022c2bf22710fbb197c46a3ca08e0d1",
 "icons/ms-icon-310x310.png": "be9497c21925e4d091ad81514e188131",
 "icons/ms-icon-70x70.png": "602874a6f063b1342f7436d9cf627ba2",
-"index.html": "13cb1e9acc9a7e1b8e2dd061d0131c9b",
-"/": "13cb1e9acc9a7e1b8e2dd061d0131c9b",
+"index.html": "f8161f652c0a7cb0e1635e1d0dbaaf6d",
+"/": "f8161f652c0a7cb0e1635e1d0dbaaf6d",
 "jsfunctions.js": "527047b4742849d7741d0e74f3aea90e",
-"main.dart.js": "20a54787513634e89800263f2f6127eb",
+"main.dart.js": "4e4576c7ff2e7ae683fad62151a33408",
 "manifest.json": "b5a8a967df78e46328f4db6abd07f563",
 "version.json": "e640fb6bb2a22b070c3f374f8c8ae980"};
 // The application shell files that are downloaded before a service worker can
