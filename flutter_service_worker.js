e921ca38a6aab6781f599a499a684522352cdf25
kalisynth <cade2002@gmail.com>
2024-09-16 09:31:42 +1000

    in2movie update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "b74ce769a1d63127e6af1454a7988dcf",
-"assets/AssetManifest.bin.json": "5b4111cb32a7a7baf1895720a62bf372",
-"assets/AssetManifest.json": "1e6252c8e885674d096966e39b77ef46",
+const RESOURCES = {"assets/AssetManifest.bin": "3eee90505fdb74b96747d209b93b1fa9",
+"assets/AssetManifest.bin.json": "7ad78b620917efec3e429487ef9fcbac",
+"assets/AssetManifest.json": "9231a4fd3e104070a972e18c3707579f",
 "assets/assets/app_strings.dart": "d41d8cd98f00b204e9800998ecf8427e",
 "assets/assets/img/bg/light_bg.png": "90580d14eb9beb97550b34d44cd40bab",
 "assets/assets/img/defaults/default_avatar.png": "d96c2c91caa2043e1da4b89e9031423d",
@@ -42,6 +42,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b74ce769a1d63127e6af1454a7988dcf
 "assets/assets/img/timetable/inclusee_tile.png": "a058fd363ec12814e2269442a4f2f5e8",
 "assets/assets/img/timetable/inclusee_tile_white.png": "3897ebd804b69b5a40c43c7757f70e24",
 "assets/assets/img/timetable/movies/In2Movies%2520-%2520A%2520Chance%2520in%2520the%2520world.png": "5b6276096c4710b54180f70e732e8437",
+"assets/assets/img/timetable/movies/In2Movies%2520-%2520Prayers%2520for%2520Bobby.png": "59f65925ac82f552024b38a3f3c4558a",
 "assets/assets/img/timetable/movies/theredpill.png": "1aa4b7f3afb2f31d7c43a9d91f412a4d",
 "assets/assets/img/timetable/specialevents/52bc6955-30f5-447a-ad7c-77e7f7a65f7f.png": "41a98eda41dece0867d52dcc0e0c63a8",
 "assets/assets/img/timetable/specialevents/Beyond%2520Blue%2520Webinar.png": "e588e51575778acf0be3c0cef446afc8",
@@ -169,10 +170,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "b74ce769a1d63127e6af1454a7988dcf
 "icons/ms-icon-150x150.png": "5022c2bf22710fbb197c46a3ca08e0d1",
 "icons/ms-icon-310x310.png": "be9497c21925e4d091ad81514e188131",
 "icons/ms-icon-70x70.png": "602874a6f063b1342f7436d9cf627ba2",
-"index.html": "731c1de39971adcda5fd7b89f2f0cc02",
-"/": "731c1de39971adcda5fd7b89f2f0cc02",
+"index.html": "80b821a5137c1379e23a0362bc50d936",
+"/": "80b821a5137c1379e23a0362bc50d936",
 "jsfunctions.js": "527047b4742849d7741d0e74f3aea90e",
-"main.dart.js": "3922e422b6de94df79ceda6394d73a15",
+"main.dart.js": "9851cfab0d49b4acbe53f5f306ff16a4",
 "manifest.json": "b5a8a967df78e46328f4db6abd07f563",
 "version.json": "e640fb6bb2a22b070c3f374f8c8ae980"};
 // The application shell files that are downloaded before a service worker can
