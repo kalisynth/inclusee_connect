c5f7bc8b0378ea22870f2a5a67fd44ae5050e64d
kalisynth <cade2002@gmail.com>
2024-11-13 12:00:35 +1000

    radio removal

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -216,10 +216,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "0555f40a35d9d0f549040ed26a75d0a1
 "icons/ms-icon-150x150.png": "5022c2bf22710fbb197c46a3ca08e0d1",
 "icons/ms-icon-310x310.png": "be9497c21925e4d091ad81514e188131",
 "icons/ms-icon-70x70.png": "602874a6f063b1342f7436d9cf627ba2",
-"index.html": "ee875a222a1ab9ce1d4d9f17cb34174c",
-"/": "ee875a222a1ab9ce1d4d9f17cb34174c",
+"index.html": "90168e13aa368052b98292164b196536",
+"/": "90168e13aa368052b98292164b196536",
 "jsfunctions.js": "527047b4742849d7741d0e74f3aea90e",
-"main.dart.js": "f4b69eafe797459000a9b10726c35108",
+"main.dart.js": "814e253f0932fc7fa6b79a294c2fe9f6",
 "manifest.json": "b5a8a967df78e46328f4db6abd07f563",
 "version.json": "e640fb6bb2a22b070c3f374f8c8ae980"};
 // The application shell files that are downloaded before a service worker can
