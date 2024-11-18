68d36c6941450675d3f73b38d7d19823948eb03d
kalisynth <cade2002@gmail.com>
2024-11-18 15:21:31 +1000

    infopage update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -176,7 +176,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "81e8d3397c40920c9bc8ba7cc9a9f131
 "assets/assets/img/timetable/weekly/Virtual%2520Field%2520Trip%2520-%2520App%2520Tile.png": "92ad4bbb21f464b4369092b459cb66bc",
 "assets/assets/img/timetable/weekly/wellnesstxt.png": "f9d53d194daa6eb3d0ec145bcbe8a7c7",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "3018bf81ee57eaad7ae6f8e6e40d3dcf",
+"assets/fonts/MaterialIcons-Regular.otf": "ab295326299defdd958949d7e941dfce",
 "assets/NOTICES": "a942760c6e779b325b50298a982ab0c6",
 "assets/packages/awesome_notifications/test/assets/images/test_image.png": "c27a71ab4008c83eba9b554775aa12ca",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
@@ -217,10 +217,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "81e8d3397c40920c9bc8ba7cc9a9f131
 "icons/ms-icon-150x150.png": "5022c2bf22710fbb197c46a3ca08e0d1",
 "icons/ms-icon-310x310.png": "be9497c21925e4d091ad81514e188131",
 "icons/ms-icon-70x70.png": "602874a6f063b1342f7436d9cf627ba2",
-"index.html": "e16409bd9284f8ab644178225815e6aa",
-"/": "e16409bd9284f8ab644178225815e6aa",
+"index.html": "0bcb19d9c056b058f19ab171a2738789",
+"/": "0bcb19d9c056b058f19ab171a2738789",
 "jsfunctions.js": "527047b4742849d7741d0e74f3aea90e",
-"main.dart.js": "4ce7936e05166e0c4672923b385939a0",
+"main.dart.js": "164daecb728fc2e4465fdcdf16786dbf",
 "manifest.json": "b5a8a967df78e46328f4db6abd07f563",
 "version.json": "e640fb6bb2a22b070c3f374f8c8ae980"};
 // The application shell files that are downloaded before a service worker can
