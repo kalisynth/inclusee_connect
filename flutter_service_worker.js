172ac498ce188597b364071ff86c1856a1d85141
kalisynth <cade2002@gmail.com>
2025-03-28 10:38:49 +1000

    hubspot test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "f93e64ef36ecad0b15d4ce06051384b9",
-"assets/AssetManifest.bin.json": "9d87b87c3e6626514449829eeb135d73",
-"assets/AssetManifest.json": "2889239653aadcedd56fcfa6b90d1504",
+const RESOURCES = {"assets/AssetManifest.bin": "85176871fcb5b1c685e3c11060404fdb",
+"assets/AssetManifest.bin.json": "828aea90e7631bcc68749078fd9d6130",
+"assets/AssetManifest.json": "555a54661e2641bae62cdaf9e1204932",
 "assets/assets/app_strings.dart": "d41d8cd98f00b204e9800998ecf8427e",
 "assets/assets/img/bg/light_bg.png": "90580d14eb9beb97550b34d44cd40bab",
 "assets/assets/img/defaults/default_avatar.png": "d96c2c91caa2043e1da4b89e9031423d",
@@ -78,6 +78,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f93e64ef36ecad0b15d4ce06051384b9
 "assets/assets/img/timetable/movies/In2Movies%2520Reduced%2520Timetable%2520-%2520Local%2520Hero%2520(PG)%2520(1).png": "6bf70bec7753679c51764147e2d57195",
 "assets/assets/img/timetable/movies/theredpill.png": "1aa4b7f3afb2f31d7c43a9d91f412a4d",
 "assets/assets/img/timetable/specialevents/52bc6955-30f5-447a-ad7c-77e7f7a65f7f.png": "41a98eda41dece0867d52dcc0e0c63a8",
+"assets/assets/img/timetable/specialevents/Aged%2520Care%2520Information%2520Series.png": "067302e51267a8c5ba15c55ba02c1b3c",
 "assets/assets/img/timetable/specialevents/Beyond%2520Blue%2520Webinar.png": "e588e51575778acf0be3c0cef446afc8",
 "assets/assets/img/timetable/specialevents/Birthday%2520Celebration%2520Club%2520Tile.png": "7d2972e5c2432800166fa09054c01a9e",
 "assets/assets/img/timetable/specialevents/Cooking%2520with%2520Kat.png": "33238dbc21d33b793b5e4a50660e3dc8",
@@ -119,6 +120,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f93e64ef36ecad0b15d4ce06051384b9
 "assets/assets/img/timetable/specialevents/Public%2520Trustee%2520Wills.png": "784570c69b53ec7f5471e7cc729ada01",
 "assets/assets/img/timetable/specialevents/scams.png": "126e8e8f3c2ee06e820379f9b96d2816",
 "assets/assets/img/timetable/specialevents/sellingsafely.png": "5fe181a3a03872eef86a4733784c0a13",
+"assets/assets/img/timetable/specialevents/Special%2520Event%2520Musical%2520Bingo.png": "3243866c7d3bb962e0da7077f4ec5099",
 "assets/assets/img/timetable/specialevents/specialEventTile.png": "e53b6f5e2db147f973c9af4805ce3040",
 "assets/assets/img/timetable/specialevents/Stroke%2520Foundation%2520Guest%2520Speaker.png": "666c4dd66e9f6815a76bbe902d00fb0c",
 "assets/assets/img/timetable/specialevents/The%2520Office%2520of%2520Fair%2520Trading%2520Guest%2520Speaker%2520App%2520tile.png": "af9fd70ab91cc1f5f60771c7080d6712",
@@ -243,10 +245,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "f93e64ef36ecad0b15d4ce06051384b9
 "icons/ms-icon-150x150.png": "5022c2bf22710fbb197c46a3ca08e0d1",
 "icons/ms-icon-310x310.png": "be9497c21925e4d091ad81514e188131",
 "icons/ms-icon-70x70.png": "602874a6f063b1342f7436d9cf627ba2",
-"index.html": "6e2b2ce93a20947194aa1ade0d3fb3d8",
-"/": "6e2b2ce93a20947194aa1ade0d3fb3d8",
+"index.html": "9405cfd94e70abee61298038f4a46a2d",
+"/": "9405cfd94e70abee61298038f4a46a2d",
 "jsfunctions.js": "527047b4742849d7741d0e74f3aea90e",
-"main.dart.js": "70aad0f1080387e85be44c230a01ecd5",
+"main.dart.js": "824adee45a0b5be41af5bdda6c279e5a",
 "manifest.json": "b5a8a967df78e46328f4db6abd07f563",
 "version.json": "e640fb6bb2a22b070c3f374f8c8ae980"};
 // The application shell files that are downloaded before a service worker can
